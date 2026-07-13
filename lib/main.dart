ba2947680f278f6e6d958ad1a32ee1625a5eac48
Sardar Fahad Ali <sardarfahadali18@gmail.com>
2026-07-14 01:46:59 +0500

    feat: initialize Firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 ﻿import 'package:flutter/material.dart';
 import 'screens/caregiver_dashboard.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,8 +21,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      ),
+      ), // ThemeData
       home: const CaregiverDashboard(),
-    );
+    ); // MaterialApp
   }
 }
